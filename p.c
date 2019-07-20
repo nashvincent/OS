f8be4e4be73f302592ea29bf591779a5ddf16db1
Nash <36297311+nashvincent@users.noreply.github.com>
2019-07-21 01:31:29 +0530

    Update p.c

--- p.c
+++ p.c
@@ -9,12 +9,12 @@
 
 int main (int argc, char *argv[]) 
 {
-	int pipefd[2];	// File descriptor of pipe
+	int pipeInfd[2];	// File descriptor of pipe
 	int pipeOutfd[2]; // File descriptor of Outgoing pipe
 	pid_t pid;	// Process ID
 	int mark[MAX_SUBJECTS];
 	
-	if (pipe(pipefd) == -1)
+	if (pipe(pipeInfd) == -1)
 	{
 		printf("Pipe Failed\n");
 		exit(EXIT_FAILURE);
@@ -32,7 +32,7 @@ int main (int argc, char *argv[])
 	{
 		scanf("%d", &mark[i]);
 	}
-	// TODO
+
 	pid = fork();
 	//printf("Hi I am %d\n", (int)getpid());
 	
@@ -42,15 +42,14 @@ int main (int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	
-	else if (pid > 0) 
+	else if (pid > 0)
 	{
 		// Parent process block
-		// TODO
 		printf("Sending student marks to child process...\n");
-		close(pipefd[0]);	// Closes the unused read end of the pipe
+		close(pipeInfd[0]);	// Closes the unused read end of the pipe
 		close(pipeOutfd[1]);
-		write(pipefd[1], mark, sizeof(mark)/sizeof(int));
-		close(pipefd[1]);
+		write(pipeInfd[1], mark, sizeof(mark)/sizeof(int));
+		close(pipeInfd[1]);
 			//printf("Parent waiting for child process to complete\n");
 		wait(NULL);
 		char *R;
@@ -59,32 +58,21 @@ int main (int argc, char *argv[])
 		printf("Result: %s\n", R);
 		printf("Parent process terminating\n");
 		
-		
-		/*
-		char result;
-		read(pipefd[0], &result, 1);
-		if (result == 'P')
-			printf("PASS\n");
-		else
-			printf("FAIL\n");
-			
-		close(pipefd[0]); */		
 	}
 	
 	else 
 	{
-		// Child process block
-		// TODO	
+		// Child process block	
 		printf("Child process recieving marks...\n");
-		close(pipefd[1]);	// Closes the unused write end of the pipe
-		close(pipeOutfd[0]);
+		close(pipeInfd[1]);	// Closes the unused write end of the pipe
+		close(pipeOutfd[0]);// Closes the unused write end of the pipe
 		int temp;
 		int sum = 0;
 		for (int j = 0; j <= MAX_SUBJECTS; j++) {
-			read(pipefd[0], &temp, sizeof(int));
+			read(pipeInfd[0], &temp, sizeof(int));
 			sum = sum + temp;
 		}
-		close(pipefd[0]);
+		close(pipeInfd[0]);
 		sum = sum/6;
 		
 		char *r;
@@ -98,6 +86,5 @@ int main (int argc, char *argv[])
 		close(pipeOutfd[1]);
 		
 		exit(0);
-	}
-	
+	}	
 }
